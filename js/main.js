60e28f88d40d1b9ca72e380c764c4cc38f2b3873
David Storey <david.storey@realise.com>
2018-11-16 11:12:35 +0000

    New design

--- js/main.js
+++ js/main.js
@@ -1,3 +1,47 @@
 (function(){
-	
+    styleConsole(
+        [
+            'Hello - have you come to look at my source-code?',
+            'You won\'t see much here.',
+            'Check out my projects on GitHub for some better examples.'
+        ],
+        {
+            'background-color': 'black',
+            'font-size': '20px',
+            'padding': '1em',
+            'color': '#007F0E',
+            'margin': '-5px',
+            'text-shadow': '0px 0px 20px rgba(150, 255, 150, 0.8)'
+        }
+    );
+
+    initTabs('about');
+
+    // Outputs styled messages to the console
+    function styleConsole(messages, styles) {
+        let str = '';
+        for (var key in styles) {
+            str += `${key}: ${styles[key]};`
+        }
+
+        messages.forEach((message) => console.log(`%c${message}`, str));
+    }
+
+    // Hide and show tab content (and handle deep linking)
+    function initTabs(initial) {
+        function showTab(id) {
+            let sections = Array.from(document.getElementsByTagName('section'));
+            sections.forEach((element) => element.style.display = 'none');
+
+            id = id.replace('#', '');
+
+            document.getElementById(id).style.display = 'block';
+        }
+
+        window.addEventListener('hashchange', () => {
+            showTab(location.hash);
+        });
+
+        showTab(location.hash ? location.hash : initial);
+    }
 }())
